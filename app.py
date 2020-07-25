683eeb7f491a9df5a2bf610c22630642da6db4f1
ss00831 <songhei_831.lee@hotmail.com>
2020-07-25 04:53:35 +0000

    Uninstall bs4 in Flask (unnecessary)

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
-from bs4 import BeautifulSoup
 if os.path.exists("env.py"):
     import env
 
